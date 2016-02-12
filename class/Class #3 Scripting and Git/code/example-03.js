bc140155f1ee4810c88d3fa8f50ed1457a279209
Stephen Cataldo <github@spaceshare.com>
2016-02-11 16:13:14 -0800

    working on class 3 a bit

--- class/Class #3 Scripting and Git/code/example-03.js
+++ class/Class #3 Scripting and Git/code/example-03.js
@@ -1,9 +1,13 @@
 function beerverse (bottleCount){
 	while (bottleCount > 0) {
+	
+	twoless= bottleCount-2;
+	
 	  var verse = bottleCount + " bottles of beer on the wall<br>"
 	    	+ bottleCount + " bottles of beer!<br>"
 	    	+ "Take one down, pass it around<br>"  
-	    	+ --bottleCount + " bottles of beer on the wall!";
+	    	+ twoless + " bottles of beer on the wall!";
+	    	bottleCount=bottleCount-2;
 	  console.log('bottlecount: ' + bottleCount);
 	 document.writeln('<p>' + verse + '</p>');
 	}
